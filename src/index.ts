3c91f7ae568311d031edce2409d125868e94c5b5
niklashans <104555311+niklashans@users.noreply.github.com>
2022-12-17 01:37:37 +0100

    Added JavaScript functionalities

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,55 @@
-import { greetUser } from '$utils/greet';
+const portfolioItems = document.querySelectorAll('[bw-portfolio-element="item"]');
+const modal = document.querySelector('[bw-portfolio-element="modal"]');
+const modalVideo = document.querySelector('[bw-portfolio-element="modal-video"]');
 
-window.Webflow ||= [];
-window.Webflow.push(() => {
-  const name = 'John Doe';
-  greetUser(name);
+// Add a button to toggle autoplay
+const toggleButton = document.querySelector('[bw-portfolio-element="allow-autoplay"]');
+
+let autoplayEnabled = false;
+toggleButton.addEventListener('click', function () {
+  autoplayEnabled = !autoplayEnabled;
+});
+
+portfolioItems.forEach(function (item) {
+  const video = item.querySelector('[bw-portfolio-element="video"]');
+  const thumbnail = item.querySelector('[bw-portfolio-element="thumbnail"]');
+  const content = item.querySelector('[bw-portfolio-element="content"]');
+
+  const fadeInDelay = 100; // 0.1 seconds
+
+  item.addEventListener('mouseenter', function () {
+    // Only play the video if autoplay is enabled
+    if (autoplayEnabled) {
+      video.play();
+    }
+    thumbnail.style.opacity = 0;
+    setTimeout(function () {
+      content.style.opacity = 0;
+    }, fadeInDelay);
+  });
+
+  item.addEventListener('mouseleave', function () {
+    video.pause();
+    thumbnail.style.opacity = 1;
+    setTimeout(function () {
+      content.style.opacity = 1;
+    }, fadeInDelay);
+  });
+
+  // Add click event listener to open modal
+  item.addEventListener('click', function () {
+    // Show modal
+    modal.style.display = 'flex';
+    // Set src of modal video based on src of portfolio item video
+    modalVideo.setAttribute('src', video.getAttribute('src'));
+    modalVideo.play();
+  });
+});
+
+// Add click event listener to close modal
+modal.addEventListener('click', function (event) {
+  if (event.target === modal || event.target.classList.contains('modal-close')) {
+    modalVideo.pause();
+    modal.style.display = 'none';
+  }
 });
